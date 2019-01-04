4fd83d962f7e2fd58de6961ed2e588f901cd5339
Francis Kayiwa <kayiwa@princeton.edu>
2019-01-04 15:40:26 -0500

    add test for user

--- roles/pulibrary.deploy-user/molecule/default/tests/test_deploy_user.py
+++ roles/pulibrary.deploy-user/molecule/default/tests/test_deploy_user.py
@@ -6,7 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_deploy_user_has_uid(host):
+def test_deploy_user_is_part_of_group(host):
     deploy_user = host.user('deploy')
 
-    assert 102 in deploy_user.uid
+    assert "deploy" in deploy_user.group
